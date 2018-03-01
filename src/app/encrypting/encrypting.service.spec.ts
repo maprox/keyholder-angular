d23abb86523bf100004444eff5f7189d2252ee4c
aintsay <aintsay@cloud.upwork.com>
2018-03-01 15:38:20 +0400

    #3 Cover encrypting service with tests

--- src/app/encrypting/encrypting.service.spec.ts
+++ src/app/encrypting/encrypting.service.spec.ts
@@ -1,15 +1,75 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
+import { AES } from 'crypto-js';
 
+import { User } from '../auth/model';
 import { EncryptingService } from './encrypting.service';
 
 describe('EncryptingService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [EncryptingService]
-    });
-  });
+    let service: EncryptingService;
 
-  it('should be created', inject([EncryptingService], (service: EncryptingService) => {
-    expect(service).toBeTruthy();
-  }));
+    beforeEach(() => {
+        sessionStorage.clear();
+
+        TestBed.configureTestingModule({
+            providers: [
+                EncryptingService
+            ]
+        });
+    });
+
+    beforeEach(inject([EncryptingService], (encryptingService: EncryptingService) => {
+        service = encryptingService;
+    }));
+
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('should return null without specified user', () => {
+        expect(service.getUser()).toBeNull();
+        expect(service.getSecret()).toBeNull();
+        expect(service.encrypt('test')).toBeNull();
+        expect(service.decrypt('test')).toBeNull();
+    });
+
+    it('should set user', () => {
+        const username = 'some@example.com';
+        const password = 'secret';
+        const user = new User(username, password);
+
+        service.setUser(user);
+        expect(service.getUser()).toEqual(user);
+    });
+
+    it('should get secret hash', () => {
+        const username = 'some@example.com';
+        const password = 'secret';
+        const user = new User(username, password);
+
+        service.setUser(user);
+
+        const hash = '7bed9582cee5a6a268b98a98214e42180c5700b135e0453351b2a4a5e033dc39';
+        expect(service.getSecret()).toEqual(hash);
+    });
+
+    it('should encrypt data', () => {
+        const username = 'some@example.com';
+        const password = 'secret';
+        const user = new User(username, password);
+
+        service.setUser(user);
+
+        const data = 'Hello virtual world!';
+        const encryptedData = 'dgdh gfdhgfd hgfdhgfdh gd h';
+
+        // for some reason original AES.encrypt and AES.decrypt fail
+        AES.encrypt = jasmine.createSpy().and.returnValue(encryptedData);
+        AES.decrypt = jasmine.createSpy().and.returnValue(data);
+
+        expect(service.encrypt(data)).toEqual(encryptedData);
+        expect(service.decrypt(encryptedData)).toEqual(data);
+
+        expect(AES.encrypt).toHaveBeenCalledWith(data, password);
+        expect(AES.decrypt).toHaveBeenCalledWith(encryptedData, password);
+    });
 });
