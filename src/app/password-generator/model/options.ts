74be0a9945afcf9f289993a0ee84c59c42d31b2a
aintsay <aintsay@cloud.upwork.com>
2018-10-23 01:11:14 +0400

    #38 Update to the latest bootstrap (v4.1)

--- src/app/password-generator/model/options.ts
+++ src/app/password-generator/model/options.ts
@@ -2,7 +2,7 @@ import { SerializableClass } from '../../serializer';
 
 export class Options extends SerializableClass {
     constructor(
-        public length: number = 20,
+        public length: number = 30,
         public useNumbers: boolean = true,
         public useSymbols: boolean = true,
         public useLowercase: boolean = true,
